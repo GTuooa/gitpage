c2e8ed9610c616edd470e9d1dcfea2c085fbd463
GTuooa <1659256785@qq.com>
2020-12-09 20:48:39 +0800

    4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('第一次提交');
 console.log('第二次提交');
 console.log('第三次提交');
 console.log('第四次提交');
-console.log('第五次提交');
-console.log('第六次提交');
-console.log('第七次提交');
+// console.log('第五次提交');
+// console.log('第六次提交');
+// console.log('第七次提交');
 
